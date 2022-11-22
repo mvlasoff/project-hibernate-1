0ae0eb3b9d3169617d23f527edf4db7d76a60216
mulasau <mikevdga@gmail.com>
2022-11-21 20:03:08 -0500

    11.21.22 Finished tasks 1-9.

--- src/main/java/com/game/entity/Player.java
+++ src/main/java/com/game/entity/Player.java
@@ -1,23 +1,36 @@
 package com.game.entity;
 
+import jakarta.persistence.*;
 import java.util.Date;
 
-
+@Entity
+@Table(schema = "rpg", name = "player")
 public class Player {
+    @Id
+    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "name", length = 12, nullable = false)
     private String name;
 
+    @Column(name = "title", length = 30, nullable = false)
     private String title;
 
+    @Column(name = "race", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Race race;
 
+    @Column(name = "profession", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
     private Profession profession;
 
+    @Column(name = "birthday", nullable = false)
     private Date birthday;
 
+    @Column(name = "banned", nullable = false)
     private Boolean banned;
 
+    @Column(name = "level", nullable = false)
     private Integer level;
 
     public Player() {
--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,17 +1,32 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
-
+        Properties properties = new Properties();
+        properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+        sessionFactory = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Player.class)
+                .buildSessionFactory();
     }
 
     @Override
